b50b2201600c48136144c9b4c1d7f132b4231d01
tanyahlynnmar02 <lynettetanyaradzwamarwisa@gmail.com>
2022-10-19 14:09:45 -0500

    musicapp

---
+++ musicapp/Lyric.py
@@ -0,0 +1,4 @@
+class Lyric:
+    def __init__(self, content, song_id):
+        self.content = content
+        self.song_id = song_id
---
+++ musicapp/Song.py
@@ -0,0 +1,7 @@
+class Song:
+    def __init__(self, title, date_released, likes, artiste_id):
+        self.title = title
+        self.date_released = date_released
+        self.likes = likes
+        self.artiste_id = artiste_id
+
