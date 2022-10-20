4a2b1b79449f365b7146c9f4a624c6e67757bf3d
tanyahlynnmar02 <lynettetanyaradzwamarwisa@gmail.com>
2022-10-19 21:15:15 -0500

    musicapp

--- musicapp/Lyric.py
+++
@@ -1,4 +0,0 @@
-class Lyric:
-    def __init__(self, content, song_id):
-        self.content = content
-        self.song_id = song_id
--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+class Artiste:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+
+class Song:
+    def __init__(self, title, date_released, likes, artiste_id):
+        self.title = title
+        self.date_released = date_released
+        self.likes = likes
+        self.artiste_id = artiste_id
+
+
+class Lyric:
+    def __init__(self, content, song_id):
+        self.content = content
+        self.song_id = song_id
 
 # Create your models here.
